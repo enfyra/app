025f61c446bf0c46b42ed5c238ddd56e172dc264
dothinh115 <dothinh115@gmail.com>
2025-12-05 02:21:59 +0700

    chore: update Nuxt configuration and CSS paths
    - Set the source directory to 'app' for better project structure.
    - Adjusted CSS paths in nuxt.config.ts to reflect the new directory layout.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  srcDir: 'app',
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   devServer: {
@@ -42,7 +43,7 @@ export default defineNuxtConfig({
     storageKey: "nuxt-color-mode",
   },
   ssr: false,
-  css: ["./app/assets/css/main.css", "./app/assets/css/highlight.css"],
+  css: ["assets/css/main.css", "assets/css/highlight.css"],
   app: {
     head: {
       htmlAttrs: {
