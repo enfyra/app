8ef585888c19fd8abffe235c4790d1de3eeeee69
dothinh115 <dothinh115@gmail.com>
2026-05-01 18:55:37 +0700

    feat: enhance AnimatedGrid component with transition handling and update CSS for item-grid leave effect

--- app/components/common/AnimatedGrid.vue
+++ app/components/common/AnimatedGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <TransitionGroup
-    :name="name"
+    :name="transitionName"
     tag="div"
     :class="['relative', gridClass]"
   >
@@ -9,11 +9,20 @@
 </template>
 
 <script setup lang="ts">
-withDefaults(defineProps<{
+const props = withDefaults(defineProps<{
   name?: string;
   gridClass?: string;
+  disabled?: boolean;
 }>(), {
   name: 'item-grid',
   gridClass: 'grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3',
+  disabled: false,
+});
+
+const routeLeaving = ref(false);
+const transitionName = computed(() => props.disabled || routeLeaving.value ? undefined : props.name);
+
+onBeforeRouteLeave(() => {
+  routeLeaving.value = true;
 });
 </script>
